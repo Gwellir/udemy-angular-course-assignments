d658f97b3e170c6dc2d6446c0ba2dffc44cebfec
Vlad Chekunov <vlad.chekunov@gmail.com>
2022-03-09 19:34:09 +0300

    add data binding demo component (username input and reset button)

--- basics-assignment-2-start/src/app/app.module.ts
+++ basics-assignment-2-start/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DataBindingComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
